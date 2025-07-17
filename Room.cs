9790a814ad3f55a73bf783798e1b4b67b0fb7252
rahmaAlmamari <alr816125@gmail.com>
2025-07-17 09:30:12 +0400

    Creating Room class wuth the needed feilds and default constructor

---
+++ Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystemOOP
+{
+    class Room
+    {
+        //1. class fields ...
+        public int RoomNumber = 0;
+        public double RoomDailyPrice;
+        public bool IsAvailable = true;
+        //2. class properties ...
+        //3. class methods ...
+        //4. class constructors ...
+        public Room()
+        {
+            RoomNumber++;
+        }
+    }
+}
