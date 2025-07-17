df614ccdd041d6e0a3be2c7c2ac342770104be32
rahmaAlmamari <alr816125@gmail.com>
2025-07-17 10:19:44 +0400

    Creating ViewAllRooms methods

--- Room.cs
+++ Room.cs
@@ -20,8 +20,44 @@ namespace HotelSystemOOP
             Room newRoom = new Room();
             newRoom.RoomDailyPrice = Validation.DoubleValidation("daily price");
             Program.HotelRooms.Add(newRoom);
-            Console.WriteLine($"Room {newRoom.RoomNumber} added successfully with daily price {newRoom.RoomDailyPrice}");
+            //Console.WriteLine($"Room {newRoom.RoomNumber} added successfully with daily price {newRoom.RoomDailyPrice}");
+            Console.WriteLine("Room add successfully with following details:");
+            Console.WriteLine(newRoom.ToString());
             Additional.HoldScreen();//to hold the screen ...
+        }
+        //to view all rooms ...
+        public static void ViewAllRooms()
+        {
+            //to check if there are no rooms available ...
+            if (Program.HotelRooms.Count == 0)
+            {
+                Console.WriteLine("No rooms available.");
+                return;
+            }
+            //to list all rooms ...
+            //to book memory place for the room details ...
+            Room RoomDetails;
+            Console.WriteLine("List of all rooms:");
+            //to loop through all rooms in the hotel ...
+            for (int i = 0; i < Program.HotelRooms.Count; i++)
+            {
+                RoomDetails = Program.HotelRooms[i];
+                //Console.WriteLine($"Room Number: {Program.HotelRooms[i].RoomNumber}\n" +
+                //                  $"Room Daily Price: {Program.HotelRooms[i].RoomDailyPrice}\n" +
+                //                  $"Room Is Available: {Program.HotelRooms[i].IsAvailable}\n");
+                //Console.WriteLine("--------------------------------------------------");
+                //to print the room details ...
+                Console.WriteLine(RoomDetails.ToString());
+                Console.WriteLine("--------------------------------------------------");
+            }
+            Additional.HoldScreen();//to hold the screen ...
+        }
+        //to print ...
+        public override string ToString()
+        {
+            return $"Room Number: {RoomNumber}\n" +
+                   $"Room Daily Price: {RoomDailyPrice}\n" +
+                   $"Room Is Available: {IsAvailable}\n";
         }
         //4. class constructors ...
         public Room()
