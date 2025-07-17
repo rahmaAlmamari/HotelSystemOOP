669aaa19dca9638fc2ad2dd31a5beb490c956d43
rahmaAlmamari <alr816125@gmail.com>
2025-07-17 09:32:43 +0400

    Adding HotelRooms and HotelGuests lists in program class

--- Program.cs
+++ Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
+        //1. HotelRooms to hold the rooms in the hotel ...
+        public static List<Room> HotelRooms = new List<Room>();
+        //2. HotelGuests to hold the guests in the hotel ...
+        public static List<Guest> HotelGuests = new List<Guest>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
