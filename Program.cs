4700b3f57b2fea64a39d2fe2689d0c9443716a74
rahmaAlmamari <alr816125@gmail.com>
2025-07-17 09:45:32 +0400

    Adding the hotel system main menu

--- Program.cs
+++ Program.cs
@@ -10,6 +10,70 @@
         {
             //to call welcome message method ...
             Additional.WelcomeMessage("Hotel");
+            //to list the main menu options ...
+            bool exitFlag = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Add new room");
+                Console.WriteLine("2. View all room");
+                Console.WriteLine("3. Reserve a room for a guest");
+                Console.WriteLine("4. View all reservations with total cost");
+                Console.WriteLine("5. Search reservation by guest name");
+                Console.WriteLine("6. Find the highest-paying guest");
+                Console.WriteLine("7. Cancel a reservation by room number ");
+                Console.WriteLine("0. Exit");
+                Console.Write("Please select an option: ");
+
+                //to get the user choice ...
+                char choice = Validation.CharValidation("option");
+                switch (choice)
+                {
+                    case '1':
+                        Console.WriteLine("1");
+                        Additional.HoldScreen();
+                        //Room.AddRoom();
+                        break;
+                    case '2':
+                        Console.WriteLine("2");
+                        Additional.HoldScreen();
+                        //Guest.AddGuest();
+                        break;
+                    case '3':
+                        Console.WriteLine("3");
+                        Additional.HoldScreen();
+                        //Room.ShowRooms();
+                        break;
+                    case '4':
+                        Console.WriteLine("4");
+                        Additional.HoldScreen();
+                        //Guest.ShowGuests();
+                        break;
+                    case '5':
+                        Console.WriteLine("5");
+                        Additional.HoldScreen();
+                        //Room.ReserveRoom();
+                        break;
+                    case '6':
+                        Console.WriteLine("6");
+                        Additional.HoldScreen();
+                        //Guest.SearchReservation();
+                        break;
+                    case '7':
+                        Console.WriteLine("7");
+                        Additional.HoldScreen();
+                        //Room.CancelReservation();
+                        break;
+                    case '0':
+                        exitFlag = true;
+                        Console.WriteLine("Thank you for using the Hotel System. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        Additional.HoldScreen();
+                        break;
+                }
+            } while (!exitFlag);
 
         }
     }
