6d91eaac3e72fdd8dd40395867df8a6e2aabfcfa
rahmaAlmamari <alr816125@gmail.com>
2025-07-17 09:30:24 +0400

    Creating Guest class wuth the needed feilds and default constructor

---
+++ Guest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystemOOP
+{
+    class Guest
+    {
+        //1. class fields ...
+        public int GuestID = 0;
+        public string GuestName;
+        public string GuestPhoneNumber;
+        public int NumberOfNights;
+        public Room GuestRoom;
+        //2. class properties ...
+        //3. class methods ...
+        //4. class constructors ...
+        public Guest()
+        {
+            GuestID++;
+        }
+    }
+}
