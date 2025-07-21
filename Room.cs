d78b0c3965d4be18cd1348f3f3295e29d9454931
rahmaAlmamari <alr816125@gmail.com>
2025-07-21 09:53:18 +0400

    Modify RoomReservations list

--- Room.cs
+++ Room.cs
@@ -14,7 +14,7 @@ namespace HotelSystemOOP
         public double RoomDailyPrice;
         public bool IsAvailable = true;
         //list to store the room reserveation ...
-        public static List<Reservation> RoomReservations = new List<Reservation>();
+        public List<Reservation> RoomReservations = new List<Reservation>();
 
         //to hold the file path for saving room details ...
         public static string filePath = "rooms.txt";
