1dc03190c05792d05f103e9bdd5541c9c0a6f82d
rahmaAlmamari <alr816125@gmail.com>
2025-07-21 09:11:28 +0400

    Create Reservation class

---
+++ Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystemOOP
+{
+    class Reservation
+    {
+        //1. class Reservation feild ...
+        public int ReservationId;
+        public int RoomNumber;
+        public string GuestName;
+        public DateOnly CheckIn;
+        public DateOnly CheckOut;
+
+        //2. class Reservation properity ...
+        //3. class Reservation method ...
+        //4. class Reservation constructor ...
+    }
+}
