44d73ee738a5146d6f03b8f37594256c5c97c502
rahmaAlmamari <alr816125@gmail.com>
2025-07-22 13:21:04 +0400

    Creating SaveReservationDetailsToFile method in reservation class

--- Reservation.cs
+++ Reservation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HotelSystemOOP
 {
-    class Reservation
+    class  Reservation
     {
         //1. class Reservation feild ...
         public static int ReservationCounter = 0; //to hold the reservation counter ...
@@ -14,6 +14,9 @@ namespace HotelSystemOOP
         public int GuestID;
         public DateOnly CheckIn;
         public DateOnly CheckOut;
+
+        //to hold file path for saving reservation details ...
+        public static string filePath = "reservation.txt";
 
         //2. class Reservation properity ...
         //3. class Reservation method ...
@@ -25,6 +28,31 @@ namespace HotelSystemOOP
                    $"Check In Date: {CheckIn}\n" +
                    $"Check Out Date: {CheckOut}\n" +
                    $"-----------------------------------------------";
+        }
+        //to save Reservation to file ...
+        public static void SaveReservationDetailsToFile()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (Room room in Program.HotelRooms)
+                    {
+                        foreach (Reservation reservation in room.RoomReservations)
+                        {
+                            writer.WriteLine(reservation.ToString());
+                        }
+                    }
+                }
+                Console.WriteLine("Reservation details saved to file successfully.");
+                Additional.HoldScreen();//just to hold second ...
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving Reservation details to file: {ex.Message}");
+                Additional.HoldScreen();//just to hold second ...
+            }
+
         }
         public void PrintReservationDetails()
         {
